271e096e10d1fc144cf29d56ed62e0bc970cb4fb
Pedro Aires <pedro.santos.1212436@sga.pucminas.br>
2023-10-22 19:48:31 -0300

    função de pirâmide etária

--- dataProcessor.py
+++ dataProcessor.py
@@ -17,10 +17,17 @@ def avgAgeCountry(data, country_name, transform_func):
     age_sum = 0
     count = 0
 
+    if(country_name == None):
+        return None
+
     for person in data:
         if person["country"] == country_name:
-            age_sum += transform_func(person["age"])
-            count += 1
+          try:
+              age_sum += transform_func(person["age"])
+              count += 1
+          except TypeError:
+              age_sum += 0
+              count += 0
 
     if count == 0:
         return None
@@ -44,3 +51,21 @@ def most_common_name_by_country(country_name, data, transform_func):
 
     most_common_name = max(names, key=names.get)
     return most_common_name
+
+def age_pyramid(country_name, data, transform_func):
+    age_intervals = [(0, 10), (11, 20), (21, 30), (31, 40), (41, 50),
+                    (51, 60), (61, 70), (71, 80), (81, 90), (91, 100), (101, float('inf'))]
+    age_counts = {interval: 0 for interval in age_intervals}
+
+    for person in data:
+        if person["country"] == country_name:
+            age = transform_func(person["age"])
+
+            if age is not None:
+                for interval in age_intervals:
+                    if interval[0] <= age <= interval[1]:
+                        age_counts[interval] += 1
+
+    ages = [age_counts[interval] for interval in age_intervals]
+
+    return ages
